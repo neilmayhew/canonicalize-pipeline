be17c16abfe5fe70255624acad47a02f1beeda8e
Neil Mayhew <neil@mayhew.name>
2025-07-11 16:34:02 -0600

    Build only executables with dynamic as well as static

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,7 +2,8 @@ final: prev:
 {
   canonicalize-pipeline = rec {
     defaultPackage = if final.stdenv.hostPlatform.isDarwin then dynamic else static;
-    dynamic = final.haskellPackages.callPackage ./default.nix {};
+    dynamic = final.haskell.lib.justStaticExecutables
+      (final.haskellPackages.callPackage ./default.nix {});
     static = final.haskell.lib.justStaticExecutables
       (final.pkgsStatic.haskellPackages.callPackage ./default.nix {});
   };
